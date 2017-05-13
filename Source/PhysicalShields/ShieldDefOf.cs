60e40c4709f617e2a6ef529f323462c2f942c161
brenthicks1 <brent.hicks@live.com>
2017-05-13 14:31:49 -0400

    screwed everything up

---
+++ Source/PhysicalShields/ShieldDefOf.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RimWorld;
+using Verse;
+
+namespace ShieldDefOf
+{
+    public class ShieldDefof : ShieldDefs
+    {
+
+        public static ShieldDef Block;
+        public static ShieldDef Bash;
+
+    }
+
+}
